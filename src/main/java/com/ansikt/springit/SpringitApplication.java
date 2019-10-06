53b286a22b3c80805af307d3d57de44614bff784
Tvrqvoise <tvrqvoise@users.noreply.github.com>
2019-10-06 14:36:31 -0500

    Enable actuator, logging

--- src/main/java/com/ansikt/springit/SpringitApplication.java
+++ src/main/java/com/ansikt/springit/SpringitApplication.java
@@ -1,30 +1,31 @@
 package com.ansikt.springit;
 
 import com.ansikt.springit.config.SpringitProperties;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
-
-    @Autowired
-    private SpringitProperties springitProperties;
+    private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
     }
 
     @Bean
-    @Profile("dev")
     CommandLineRunner runner() {
         return args -> {
-            System.out.println("This is something we'd only do in dev!");
+            log.error("CommandLineRunner.run();");
+            log.warn("CommandLineRunner.run();");
+            log.info("CommandLineRunner.run();");
+            log.debug("CommandLineRunner.run();");
+            log.trace("CommandLineRunner.run();");
         };
     }
 }
