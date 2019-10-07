39db43c36a7ba57610bf20ed8e347cba91dcf3a5
tvrqvoise <tvrqvoise@users.noreply.github.com>
2019-10-06 21:47:05 -0500

    Added / Integrated Lombok

---
+++ src/main/java/com/ansikt/springit/domain/Comment.java
@@ -0,0 +1,19 @@
+package com.ansikt.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String body;
+}
---
+++ src/main/java/com/ansikt/springit/domain/Link.java
@@ -0,0 +1,20 @@
+package com.ansikt.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Link {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String url;
+}
